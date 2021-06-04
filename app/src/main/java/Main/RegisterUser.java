03e62de183f0bad047955baf3f3e073bee7f54fe
xeals <xxZeroxxism@yahoo.co.uk>
2021-06-04 20:36:13 +0100

    Added user registration file and linked firebase authentication dependencies

---
+++ app/src/main/java/Main/RegisterUser.java
@@ -0,0 +1,16 @@
+package Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.Main.R;
+
+public class RegisterUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+}
